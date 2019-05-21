7c8adf7dad24d9781ffc359ef003fc6e9e2ae24c
Kamil <Kamil.Mag1990@gmail.com>
2019-05-21 11:24:27 +0300

    change js

--- js/my_js.js
+++ js/my_js.js
@@ -2,3 +2,6 @@
 //   var messege = document.getElementById('messege');
 //   window.alert(messege.value);
 // }, 4000);
+var tegTest = document.getElementsByTagName("li");
+console.log(tegTest);
+tegTest.style.color = 'red';
